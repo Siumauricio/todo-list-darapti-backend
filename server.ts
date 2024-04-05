c3894c5144f537528ec57cb0785d8ed555a20759
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-04-05 11:14:30 -0600

    #comment setting up typescript, tsx, prettier and eslint #time 40m

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
